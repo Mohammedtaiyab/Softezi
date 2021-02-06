eecef00a743aa506bce9db35128093a6d1878578
Mohammedtaiyab <55629150+Mohammedtaiyab@users.noreply.github.com>
2021-02-06 20:56:41 +0530

    contact

--- contact.php
+++ contact.php
@@ -1,3 +1,53 @@
+<?php
+require "PHPMailer/PHPMailerAutoload.php";
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+function smtpmailer($to, $from, $from_name, $subject, $body)
+    {
+        $mail = new PHPMailer();
+        $mail->IsSMTP();
+        $mail->SMTPAuth = true; 
+ 
+        $mail->SMTPSecure = 'SMTP'; 
+        $mail->Host = 'smtp.hostinger.in';
+        $mail->Port = 587;  
+        $mail->Username = 'support@softezi.in';
+        $mail->Password = 'SoftEzi@123';   
+   
+   //   $path = 'reseller.pdf';
+   //   $mail->AddAttachment($path);
+   
+        $mail->IsHTML(true);
+        $mail->From="support@softezi.in";
+        $mail->FromName=$from_name;
+        $mail->Sender=$from;
+        $mail->AddReplyTo($from, $from_name);
+        $mail->Subject = $subject;
+        $mail->Body = $body;
+        $mail->AddAddress($to);
+        if(!$mail->Send())
+        {
+            $error ="Please try Later, Error Occured while Processing...";
+            return $error; 
+        }
+        else 
+        {
+            $error = "Thanks You !! Your email is sent.";  
+            return $error;
+        }
+    }
+    
+    $to   = 'mohammedbori44@gmail.com';
+    $from = 'support@softezi.in';
+    $name = 'SoftEzi';
+    $subj = 'PHPMailer 5.2 testing from DomainRacer';
+    $msg = 'This is mail about testing mailing using PHP.';
+    
+    $error=smtpmailer($to,$from, $name ,$subj, $msg);
+    
+?>
+
  <!DOCTYPE html>
 <html lang="en-US">
 <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
